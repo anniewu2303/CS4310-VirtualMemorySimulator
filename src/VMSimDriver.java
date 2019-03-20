b2bb2e4e492f839490b5cc77f689cb1175507d8d
Annie Wu <anniewu2303@gmail.com>
2019-03-20 14:17:05 -0700

    Update VMSimDriver.java

--- src/VMSimDriver.java
+++ src/VMSimDriver.java
@@ -1,9 +1,35 @@
-/**
- * @author Joshua Chen
- * Date Created: Mar 14, 2019
- */
+/*
+ Author:        Joshua Chen
+                Annie Wu 
+				
+ Assignment:    Program 2 
+ Class:         CS 4310 - Operating Systems 
+ Instructor:    Dominick Atanasio 
+ Date:          24 March 2019 
+ 
+ 
+Your program will accept as a command line argument the test file path to a text file populated with virtual memory
+addresses (in hex and also provided with this project) that are used to simulate memory accesses called by a process.
+
+- For each test file that you run, you should use an original copy of the page files that are available with the project. 
+- You do not want altered page files of a previous runs to be used. 
+- Keep the originals in a safe place and overwrite the working set for each simulation. 
+- This should be done programmatically from within your simulator, not manually. 
+- You should use a file copy facility of your chosen language, not an operating system call. 
+- You cannot assume that the operating system of the grader is the same as yours.
+
+*/
+
+import java.io.*;
+
 public class VMSimDriver {
     public static void main(String[] args) {
-
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter location of your input file: ");
+        String file = scan.nextLine();
+        scan.close();
+        
     }
 }
