b9f607bd5a6ef945d434bf565e574d3e621d11f5
Annie Wu <anniewu2303@gmail.com>
2019-03-20 14:17:27 -0700

    Update TLB.java

--- src/TLB.java
+++ src/TLB.java
@@ -8,10 +8,10 @@
  Date:          24 March 2019 
  
  
-The TLB will be a one-dimensional array if TlbEntries. 
-A TlbEntry consists of a virtual page number and a frame number. 
-The TLB is small and must be scanned on every lookup. 
-The arrays used to implement the page table and TLB will be arrays of data structures that represent the tables’ entries.
+- The TLB will be a one-dimensional array if TlbEntries. 
+- A TlbEntry consists of a virtual page number and a frame number. 
+- The TLB is small and must be scanned on every lookup. 
+- The arrays used to implement the page table and TLB will be arrays of data structures that represent the tables’ entries.
 
 Table entry for the TLB:
     | V-Page# | V | R | D | PageFrame# |
