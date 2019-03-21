889c5b2545d1c9f32fc6d103c1ae364d1f79402d
joshuachen1 <josh526chen@gmail.com>
2019-03-21 12:30:58 -0700

    Finished Read and Write of Files

--- src/VMSimDriver.java
+++ src/VMSimDriver.java
@@ -26,7 +26,8 @@ public class VMSimDriver {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Scanner scan = new Scanner(System.in);
 
-        createWorkingSet("page_files");
+        String pageFilesPath = "page_files";
+        createWorkingSet(pageFilesPath);
 
         // Physical Memory:
         // 2^12 - 2^8 = 2^4 = 16 Pages
@@ -45,52 +46,71 @@ public class VMSimDriver {
         String testFilePath = scan.next();
         Scanner testFile = new Scanner(new File(testFilePath));
 
-        String[] parseFile = testFilePath.split("/");
-        String logName = parseFile[parseFile.length - 1] + "_log.txt";
-        PrintWriter log = new PrintWriter(logName, "UTF-8");
+        PrintWriter log = new PrintWriter("log.txt", "UTF-8");
 
         while (testFile.hasNextLine()) {
             int rw = Integer.parseInt(testFile.nextLine());
             String address = testFile.nextLine();
 
-            String pageFileName;
+            String pageFileName, pageFrame;
             Scanner page;
-
-            String pageFrame;
-            int pageIndex;
-            int currValue = 0;
-            int newValue;
+            int pageIndex, currValue = 0, newValue;
 
             switch (rw) {
                 // Read
                 case 0:
                     pageFrame = address.substring(0, 2);
-                    pageIndex = Integer.parseInt(address.substring(2), 16);
+                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
 
-                    pageFileName = "page_files/" + pageFrame;
+                    pageFileName = pageFilesPath + "_working_set/" + pageFrame;
                     page = new Scanner(new File(pageFileName + ".pg"));
 
                     for (int i = 0; page.hasNextLine(); i++) {
                         if (i == pageIndex) {
                             currValue = Integer.parseInt(page.nextLine());
+                            break;
                         }
                         page.nextLine();
                     }
-
-                    log.printf("Read Page Frame %2s, Index %2s: %d\n", pageFrame, pageIndex, currValue);
+                    log.printf("Read --  %s.pg Index (%s/%d): %d\n", address.substring(0,2), address.substring(2,4), pageIndex, currValue);
                     break;
                 // Write
                 case 1:
                     pageFrame = address.substring(0, 2);
-                    pageIndex = Integer.parseInt(address.substring(2), 16);
+                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
                     newValue = Integer.parseInt(testFile.nextLine());
+
+                    pageFileName = pageFilesPath + "_working_set/" + pageFrame;
+
+                    BufferedReader reader = new BufferedReader(new FileReader(pageFileName + ".pg"));
+                    StringBuffer strBuffer = new StringBuffer();
+                    String line;
+                    int lineNum = 0;
+                    while ((line = reader.readLine()) != null) {
+                        if (lineNum == pageIndex - 1) {
+                            currValue = Integer.parseInt(line);
+                            strBuffer.append(newValue + "\n");
+                        } else {
+                            strBuffer.append(line + "\n");
+                        }
+                        lineNum += 1;
+                    }
+                    String inputStr = strBuffer.toString();
+                    reader.close();
+
+                    FileOutputStream writeToFile = new FileOutputStream(pageFileName + ".pg");
+                    writeToFile.write(inputStr.getBytes());
+                    writeToFile.close();
+
+                    log.printf("Write --  %s.pg Index (%s/%d): (%d --> %d)\n", address.substring(0,2), address.substring(2,4), pageIndex, currValue, newValue);
+                    break;
             }
         }
-
     }
 
     /**
      * Create Working Set Directory with all path files copied
+     *
      * @param path
      * @throws IOException
      */
@@ -108,9 +128,7 @@ public class VMSimDriver {
         // Get all files from the path directory
         File[] files = pathDirectory.listFiles();
         for (File f : files) {
-            String[] fileName = f.getName().split("\\.");
-
-            File copyFile = new File(workSetDirectoryName + "/" + fileName[0] + ".txt");
+            File copyFile = new File(workSetDirectoryName + "/" + f.getName());
             PrintWriter output = new PrintWriter(copyFile);
 
             // Copy Contents of Original Files to new Files
