f3b14f0b3467f5f27be2181caf43ce6c7a6b480f
Annie Wu <anniewu2303@gmail.com>
2019-03-22 17:08:40 -0700

    check TLB for page number

--- src/TLB.java
+++ src/TLB.java
@@ -1,64 +1,67 @@
 /**
- Author:        Joshua Chen
-                Annie Wu 
-				
- Assignment:    Program 2 
- Class:         CS 4310 - Operating Systems 
- Instructor:    Dominick Atanasio 
- Date:          24 March 2019 
- 
- 
-- The TLB will be a one-dimensional array if TlbEntry.
-- A TlbEntry consists of a virtual page number and a frame number. 
-- The TLB is small and must be scanned on every lookup. 
-- The arrays used to implement the page table and TLB will be arrays of data structures that represent the tables’ entries.
-
-Table entry for the TLB:
-    | V-Page# | V | R | D | PageFrame# |
+ * @author:        Joshua Chen
+ *                  Annie Wu 
+ * 
+ * Assignment:      Program 2 
+ * Class:           CS 4310 - Operating Systems 
+ * Instructor:      Dominick Atanasio 
+ * Date:            24 March 2019 
+ * 
+ * 
+* - The TLB will be a one-dimensional array if TlbEntry.
+* - A TlbEntry consists of a virtual page number and a frame number. 
+* - The TLB is small and must be scanned on every lookup. 
+* - The arrays used to implement the page table and TLB will be arrays of data structures that represent the tables’ entries.
+* 
+* Table entry for the TLB:
+*       | V-Page# | V | R | D | PageFrame# |
 */
 
 public class TLB {
     private TlbEntry tlbEntries[];
     private int currEntry;
     private final static int DEFAULT_MAX_ENTRIES = 16;
-
+    
     public TLB() {
         this(DEFAULT_MAX_ENTRIES);
     }
 
     public TLB(int maxEntries) {
-        this.tlbEntries = new TlbEntry[DEFAULT_MAX_ENTRIES];
+        this.tlbEntries = new TlbEntry[maxEntries];
         this.currEntry = 0;
     }
 
-    /*
-    Initialize TLB
-    */
-    public void start() {
-
+    /**
+     * Check TLB for page frame number
+     * @param pageNumber
+     * @return frameNumber if it exists
+     */
+    public int checkTLB(int pageNumber) {
+        int frameNumber = -1;
+        
+        for (int i=0; i<DEFAULT_MAX_ENTRIES; i++) {
+            if (tlbEntries[i].getPageNumber == pageNumber && tlbEntries[i].isValid()) {
+                frameNumber = tlbEntries[i].getPageFrameNumber();
+            }
+        }
+        
+        return frameNumber;
     }
 
-    /*
-    Check if page number is in the TLB. 
-	*/
-    public int checkPageNumber(int pageNumber) {
-        return 0;
-    }
-
-    /*
-    Update this page number in TLB with this frame number
-    */
+    /**
+     * Update this page number in TLB with this frame number
+     * @param pageNumber
+     * @param frameNumber 
+     */
     public void update(int pageNumber, int frameNumber) {
-
+        
     }
 
-    /*
-    Update dirty bit
-    */
-    public void updateDirtyBit(int pageNumber) {
-
-    }
-
+    /**
+     * Add entry to TLB 
+     * @param vpn
+     * @param pageFrameNum 
+     */
     public void addEntry(String vpn, int pageFrameNum) {
         tlbEntries[currEntry] = new TlbEntry(vpn, pageFrameNum);
 
