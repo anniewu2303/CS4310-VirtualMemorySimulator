00a0df55383e236279b2cc544abc831e1837be99
Annie Wu <anniewu2303@gmail.com>
2019-03-23 18:44:31 -0700

    update javadoc format

--- src/CSV.java
+++ src/CSV.java
@@ -1,26 +1,26 @@
 /**
- Author:        Joshua Chen
-                Annie Wu 
-				
- Assignment:    Program 2 
- Class:         CS 4310 - Operating Systems 
- Instructor:    Dominick Atanasio 
- Date:          24 March 2019 
- 
-- Your program will output the following information in a CSV file (with appropriate headers):
-	- The address, 
-	- Read or write (0 or 1),
-	- The value read or written, 
-	- Soft miss (0 = false, 1 = true), 
-	- Hard miss (0 = false, 1 = true), 
-	- A hit (0 = false, 1 = true), 
-	- page number of the evicted page, 
-	- was that page’s dirty bit set.
-- The header of the CSV should be:
-	Address, r/w, value, soft, hard, hit, evicted_pg#, dirty_evicted_page
-- There should be one csv for every test file.
-
-*/
+ * Author:        Joshua Chen
+ *                Annie Wu 
+ * 
+ * Assignment:    Program 2 
+ * Class:         CS 4310 - Operating Systems 
+ * Instructor:    Dominick Atanasio 
+ * Date:          24 March 2019 
+ *
+ * - Your program will output the following information in a CSV file (with appropriate headers):
+ *      - The address, 
+ *      - Read or write (0 or 1),
+ *      - The value read or written, 
+ *      - Soft miss (0 = false, 1 = true), 
+ *      - Hard miss (0 = false, 1 = true), 
+ *      - A hit (0 = false, 1 = true), 
+ *      - page number of the evicted page, 
+ *      - was that page’s dirty bit set.
+ * - The header of the CSV should be:
+ *      Address, r/w, value, soft, hard, hit, evicted_pg#, dirty_evicted_page
+ * - There should be one csv for every test file.
+ * 
+ */
 
 import java.io.*;
 
