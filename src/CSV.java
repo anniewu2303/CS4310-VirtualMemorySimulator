89a6772b07bb7f5235fb60372e99c434f65a8bed
Annie Wu <anniewu2303@gmail.com>
2019-03-24 14:39:50 -0700

    Update Javadoc

--- src/CSV.java
+++ src/CSV.java
@@ -1,24 +1,20 @@
 /**
- * Author:        Joshua Chen
- * Annie Wu
+ * @author: Joshua Chen, Annie Wu
+ * @date Mar 24, 2019
  * <p>
- * Assignment:    Program 2
- * Class:         CS 4310 - Operating Systems
- * Instructor:    Dominick Atanasio
- * Date:          24 March 2019
+ * Assignment:  Project 2 - Virtual Memory Simulator
+ * Class:       CS 4310 - Operating Systems
+ * Instructor:  Dominick Atanasio
  * <p>
- * - Your program will output the following information in a CSV file (with appropriate headers):
- * - The address,
- * - Read or write (0 or 1),
- * - The value read or written,
- * - Soft miss (0 = false, 1 = true),
- * - Hard miss (0 = false, 1 = true),
- * - A hit (0 = false, 1 = true),
- * - page number of the evicted page,
- * - was that page’s dirty bit set.
- * - The header of the CSV should be:
- * Address, r/w, value, soft, hard, hit, evicted_pg#, dirty_evicted_page
- * - There should be one csv for every test file.
+ * CSV Output File Contains:
+ * - Address
+ * - Read or Write (0 or 1)
+ * - Value that is Read or Written
+ * - Soft miss (0 = false, 1 = true)
+ * - Hard miss (0 = false, 1 = true)
+ * - Hit (0 = false, 1 = true)
+ * - Page Number of the Evicted Page
+ * - Dirty Bit of the Page Number
  */
 
 import java.io.FileNotFoundException;
@@ -133,8 +129,9 @@ public class CSV {
     public void output() {
         if (address != null && rw != null && value != null && soft != null && hard != null &&
                 hit != null && evictedPageNumber != null && dirtyEvictedPage != null) {
+
             csv.println(address + ", " + rw + ", " + value + ", " + soft + ", " + hard + ", " +
-                    hit + ", " + evictedPageNumber + ", " + dirtyEvictedPage);
+                        hit + ", " + evictedPageNumber + ", " + dirtyEvictedPage);
 
             address = null;
             rw = null;
