9f6de6d9f295b7025912a7b569541ef289cf4e30
joshuachen1 <josh526chen@gmail.com>
2019-03-24 12:50:15 -0700

    Improved JavaDoc

--- src/VMSimDriver.java
+++ src/VMSimDriver.java
@@ -1,22 +1,17 @@
 /**
- * @author: Joshua Chen
- * Annie Wu
- * <p>
- * Assignment:    Program 2
- * Class:         CS 4310 - Operating Systems
- * Instructor:    Dominick Atanasio
- * Date:          24 March 2019
- * <p>
- * <p>
- * Your program will accept as a command line argument the test file path to a text file populated with virtual memory
- * addresses (in hex and also provided with this project) that are used to simulate memory accesses called by a process.
- * <p>
- * - For each test file that you run, you should use an original copy of the page files that are available with the project.
- * - You do not want altered page files of a previous runs to be used.
- * - Keep the originals in a safe place and overwrite the working set for each simulation.
- * - This should be done programmatically from within your simulator, not manually.
- * - You should use a file copy facility of your chosen language, not an operating system call.
- * - You cannot assume that the operating system of the grader is the same as yours.
+ * @author:     Joshua Chen, Annie Wu
+ * @date        Mar 24, 2019
+ *
+ * Assignment:  Project 2 - Virtual Memory Simulator
+ * Class:       CS 4310 - Operating Systems
+ * Instructor:  Dominick Atanasio
+ *
+ * Accepts a command line argument for a test file path to a text file populated with virtual memory address.
+ * Ex. test_files/test_1.txt
+ *
+ * Page Files Acting as the Physical Memory Addresses Will Have a Working Set Copy Created.
+ * This way no altered page files of a previous run is used.
+ *
  */
 
 import java.io.*;
@@ -37,8 +32,7 @@ public class VMSimDriver {
     }
 
     /**
-     * Create Working Set Directory with all path files copied
-     *
+     * Create Working Set Directory with all path files copied.
      * @param path
      * @throws IOException
      */
