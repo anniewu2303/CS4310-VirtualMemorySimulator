6e3def79c7c4db4aadd8d6031dfb291dc228ce91
joshuachen1 <josh526chen@gmail.com>
2019-03-23 22:20:07 -0700

    Reconstructed File Operations

--- src/CPU.java
+++ src/CPU.java
@@ -1,63 +1,140 @@
-///*
-// Author:        Joshua Chen
-//                Annie Wu
-//
-// Assignment:    Program 2
-// Class:         CS 4310 - Operating Systems
-// Instructor:    Dominick Atanasio
-// Date:          24 March 2019
-//
-//
-//- Your program will accept as a command line argument the test file path to a text file populated with virtual memory
-//addresses (in hex and also provided with this project) that are used to simulate memory accesses called by a process.
-//
-//- Your CPU will read them, hand them to the MMU for fetching or writing.
-//    - If the address is preceded by a zero, then the MMU should only read value.
-//    - If the address is preceded by a one, then the address will be followed by an integer value that needs to be
-//    written to physical memory.
-//
-//*/
-//
-//import java.io.*;
-//import java.util.*;
-//
-//public class CPU {
-//
-//    public void readFile(String fileName) throws FileNotFoundException {
-//        File file = new File(fileName);
-//        Scanner scan = new Scanner(file);
-//        int counter = 0; //counter for number of instructions
-//
-//        while (scan.hasNextInt()) {
-//            int rw = scan.nextInt(); //get read/write bit
-//            CSV.rw(rw);
-//
-//            if (rw == 0) { //read value
-//                String address = scan.next();
-//                CSV.address(address);
-//		        int value = MMU.read(address);
-//                CSV.value(value);
-//                counter++;
-//            }
-//
-//            if (rw == 1) { //write value
-//                String address = scan.next();
-//                CSV.address(address);
-//                int value = scan.nextInt();
-//                CSV.value(value);
-//                MMU.write(address, value);
-//                counter++;
-//            }
-//
-//            //OS resets r-bits every 10 instructions
-//            if (counter >= 10){
-//                OS.resetR();
-//                counter = 0;
-//            }
-//
-//            CSV.output();
-//        }
-//
-//    scan.close();
-//    }
-//}
+/*
+ Author:        Joshua Chen
+                Annie Wu
+
+ Assignment:    Program 2
+ Class:         CS 4310 - Operating Systems
+ Instructor:    Dominick Atanasio
+ Date:          24 March 2019
+
+
+- Your program will accept as a command line argument the test file path to a text file populated with virtual memory
+addresses (in hex and also provided with this project) that are used to simulate memory accesses called by a process.
+
+- Your CPU will read them, hand them to the MMU for fetching or writing.
+    - If the address is preceded by a zero, then the MMU should only read value.
+    - If the address is preceded by a one, then the address will be followed by an integer value that needs to be
+    written to physical memory.
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class CPU {
+    private String pageFilesPath;
+    private MMU mmu;
+    private TLB tlb;
+
+    public CPU(String pageFilesPath) throws IOException {
+        this.mmu = new MMU();
+        this.tlb = new TLB(16);
+        this.pageFilesPath = pageFilesPath;
+    }
+
+    public void readFile(String testFilePath) throws IOException {
+        Scanner testFile  = new Scanner(new File(testFilePath));
+
+        createWorkingSet(this.pageFilesPath);
+
+        PrintWriter log = new PrintWriter("log.txt", "UTF-8");
+
+        while (testFile.hasNextLine()) {
+            int rw = Integer.parseInt(testFile.nextLine());
+            String address = testFile.nextLine();
+
+            String pageFileName, pageFrame;
+            Scanner page;
+            int pageIndex, currValue = 0, newValue;
+
+            switch (rw) {
+                // Read
+                case 0:
+                    pageFrame = address.substring(0, 2);
+                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
+
+                    // Page to Read From
+                    pageFileName = pageFilesPath + "_working_set/" + pageFrame + ".pg";
+                    page = new Scanner(new File(pageFileName));
+
+                    for (int i = 0; page.hasNextLine(); i++) {
+                        if (i == pageIndex) {
+                            currValue = Integer.parseInt(page.nextLine());
+                            break;
+                        }
+                        page.nextLine();
+                    }
+                    log.printf("Read --  %s.pg Index (%s/%d): %d\n",
+                            address.substring(0, 2), address.substring(2, 4), pageIndex, currValue);
+                    break;
+                // Write
+                case 1:
+                    pageFrame = address.substring(0, 2);
+                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
+                    newValue = Integer.parseInt(testFile.nextLine());
+
+                    // Page to Write To
+                    pageFileName = pageFilesPath + "_working_set/" + pageFrame + ".pg";
+
+                    BufferedReader reader = new BufferedReader(new FileReader(pageFileName));
+                    StringBuffer strBuffer = new StringBuffer();
+                    String line;
+                    int lineNum = 0;
+                    while ((line = reader.readLine()) != null) {
+                        // Replace Line with New Value
+                        if (lineNum == pageIndex - 1) {
+                            currValue = Integer.parseInt(line);
+                            strBuffer.append(newValue + "\n");
+                        } else {
+                            strBuffer.append(line + "\n");
+                        }
+                        lineNum += 1;
+                    }
+                    String inputStr = strBuffer.toString();
+                    reader.close();
+
+                    FileOutputStream writeToFile = new FileOutputStream(pageFileName);
+                    writeToFile.write(inputStr.getBytes());
+                    writeToFile.close();
+
+                    log.printf("Write --  %s.pg Index (%s/%d): (%d --> %d)\n",
+                            address.substring(0, 2), address.substring(2, 4), pageIndex, currValue, newValue);
+                    break;
+                default:
+                    System.out.println("Problem With File Format");
+            }
+        }
+    }
+
+    /**
+     * Create Working Set Directory with all path files copied
+     *
+     * @param path
+     * @throws IOException
+     */
+    private void createWorkingSet(String path) throws IOException {
+        File pathDirectory = new File(path);
+
+        String workSetDirectoryName = path.concat("_working_set");
+
+        // Create Directory if it does not exist
+        File directory = new File(workSetDirectoryName);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        // Get all files from the path directory
+        File[] files = pathDirectory.listFiles();
+        for (File f : files) {
+            File copyFile = new File(workSetDirectoryName + "/" + f.getName());
+            PrintWriter output = new PrintWriter(copyFile);
+
+            // Copy Contents of Original Files to new Files
+            Scanner file = new Scanner(new File(path + "/" + f.getName()));
+            while (file.hasNextLine()) {
+                output.println(file.nextLine());
+            }
+            output.close();
+        }
+    }
+}
--- src/VMSimDriver.java
+++ src/VMSimDriver.java
@@ -23,12 +23,7 @@ import java.io.*;
 import java.util.Scanner;
 
 public class VMSimDriver {
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        Scanner scan = new Scanner(System.in);
-
-        String pageFilesPath = "page_files";
-        createWorkingSet(pageFilesPath);
-
+    public static void main(String[] args) throws IOException {
         // Physical Memory:
         // 2^12 - 2^8 = 2^4 = 16 Pages
         // Each pages contains 12 bits addressable width
@@ -38,112 +33,12 @@ public class VMSimDriver {
         // 2^16 - 2^8 = 2^8 = 256 Page Table Entries
         PageTable pageTable = new PageTable(256);
 
-        // TLB:
-        TLB tlb = new TLB(16);
+        CPU cpu = new CPU("page_files");
 
         // ie. test_files/test_1.txt
+        Scanner scan = new Scanner(System.in);
         System.out.print("Test File Path: ");
         String testFilePath = scan.next();
-        Scanner testFile = new Scanner(new File(testFilePath));
-
-        PrintWriter log = new PrintWriter("log.txt", "UTF-8");
-
-        while (testFile.hasNextLine()) {
-            int rw = Integer.parseInt(testFile.nextLine());
-            String address = testFile.nextLine();
-
-            String pageFileName, pageFrame;
-            Scanner page;
-            int pageIndex, currValue = 0, newValue;
-
-            switch (rw) {
-                // Read
-                case 0:
-                    pageFrame = address.substring(0, 2);
-                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
-
-                    // Page to Read From
-                    pageFileName = pageFilesPath + "_working_set/" + pageFrame + ".pg";
-                    page = new Scanner(new File(pageFileName));
-
-                    for (int i = 0; page.hasNextLine(); i++) {
-                        if (i == pageIndex) {
-                            currValue = Integer.parseInt(page.nextLine());
-                            break;
-                        }
-                        page.nextLine();
-                    }
-                    log.printf("Read --  %s.pg Index (%s/%d): %d\n",
-                            address.substring(0, 2), address.substring(2, 4), pageIndex, currValue);
-                    break;
-                // Write
-                case 1:
-                    pageFrame = address.substring(0, 2);
-                    pageIndex = Integer.parseInt(address.substring(2), 16); // Convert hex String to Int
-                    newValue = Integer.parseInt(testFile.nextLine());
-
-                    // Page to Write To
-                    pageFileName = pageFilesPath + "_working_set/" + pageFrame + ".pg";
-
-                    BufferedReader reader = new BufferedReader(new FileReader(pageFileName));
-                    StringBuffer strBuffer = new StringBuffer();
-                    String line;
-                    int lineNum = 0;
-                    while ((line = reader.readLine()) != null) {
-                        // Replace Line with New Value
-                        if (lineNum == pageIndex - 1) {
-                            currValue = Integer.parseInt(line);
-                            strBuffer.append(newValue + "\n");
-                        } else {
-                            strBuffer.append(line + "\n");
-                        }
-                        lineNum += 1;
-                    }
-                    String inputStr = strBuffer.toString();
-                    reader.close();
-
-                    FileOutputStream writeToFile = new FileOutputStream(pageFileName);
-                    writeToFile.write(inputStr.getBytes());
-                    writeToFile.close();
-
-                    log.printf("Write --  %s.pg Index (%s/%d): (%d --> %d)\n",
-                            address.substring(0, 2), address.substring(2, 4), pageIndex, currValue, newValue);
-                    break;
-                default:
-                    System.out.println("Problem With File Format");
-            }
-        }
-    }
-
-    /**
-     * Create Working Set Directory with all path files copied
-     *
-     * @param path
-     * @throws IOException
-     */
-    private static void createWorkingSet(String path) throws IOException {
-        File pathDirectory = new File(path);
-
-        String workSetDirectoryName = path.concat("_working_set");
-
-        // Create Directory if it does not exist
-        File directory = new File(workSetDirectoryName);
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-
-        // Get all files from the path directory
-        File[] files = pathDirectory.listFiles();
-        for (File f : files) {
-            File copyFile = new File(workSetDirectoryName + "/" + f.getName());
-            PrintWriter output = new PrintWriter(copyFile);
-
-            // Copy Contents of Original Files to new Files
-            Scanner file = new Scanner(new File(path + "/" + f.getName()));
-            while (file.hasNextLine()) {
-                output.println(file.nextLine());
-            }
-            output.close();
-        }
+        cpu.readFile(testFilePath);
     }
 }
